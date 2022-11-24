35c083f9f965b61202e3a21e666a76df6375886e
Ross Motley <ross.motley@gmail.com>
2022-11-24 13:47:34 +0000

    Add cookieyes cookie banner

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
+html_static_path = ["js"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)

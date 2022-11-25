ac98ef612ef147dfc2027bc56bed9b965d092763
Ross Motley <ross.motley@gmail.com>
2022-11-25 15:01:23 +0000

    Only send ids once

--- docs/source/_static/js/init.js
+++ docs/source/_static/js/init.js
@@ -6,15 +6,18 @@ document.head.appendChild(script);
 
 // Track scrolling to h2 sections by id
 (function () {
+  var sentIds = [];
+
   const observerCallback = (entries, observer, h2) => {
     entries.forEach((entry, i) => {
       if (entry.isIntersecting) {
         // Get parent <section> of the h2, which has an id we can use to track
         var id = h2.closest("section").getAttribute("id");
-        if (id && gtag) {
+        if (id && gtag && !sentIds.includes(id)) {
           gtag("event", "doc_section_view", {
-            "doc_section_view_id": id
+            doc_section_view_id: id,
           });
+          sentIds.push(id);
         }
       }
     });
